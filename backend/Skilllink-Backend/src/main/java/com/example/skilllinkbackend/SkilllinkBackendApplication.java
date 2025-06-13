e8f329ac06bba0ccae54f6a245cd937acda81360
Yanes3 <cye13766@hotmial.com>
2025-06-13 10:24:21 -0500

    feat(backend): initialize basic project structure

---
+++ backend/Skilllink-Backend/src/main/java/com/example/skilllinkbackend/SkilllinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.skilllinkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilllinkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkilllinkBackendApplication.class, args);
+    }
+
+}
---
+++ backend/Skilllink-Backend/src/test/java/com/example/skilllinkbackend/SkilllinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skilllinkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilllinkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
