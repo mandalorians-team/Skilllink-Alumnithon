3ab3831f2933a0b0eaa650da208263f505b3d0ed
Alejandro <alejodev2022@gmail.com>
2025-06-22 11:15:43 -0500

    refactor(main.jsx): actualizar la estructura de renderizado del componente principal, incorporando AuthProvider y mejorando la gestión de rutas con BrowserRouter.

--- frontend/SkillLink-Frontend/src/main.jsx
+++ frontend/SkillLink-Frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import React from "react";
 import App from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
