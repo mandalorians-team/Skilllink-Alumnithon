d7e990ed59e30d60f81b32e592186a845cb4985d
Alejandro <alejodev2022@gmail.com>
2025-06-23 12:28:20 -0500

    feat(App): agregar nuevas rutas y componentes para el dashboard, cursos, mentorías y proyectos, mejorando la estructura de navegación de la aplicación.

--- frontend/SkillLink-Frontend/src/App.jsx
+++ frontend/SkillLink-Frontend/src/App.jsx
@@ -11,17 +11,31 @@ import Sidebar from "./components/comun/Sidebar";
 import { useLocation } from "react-router-dom";
 import CurseTabs from "./components/Aprendiz/Curso/CourseTabs";
 import AppRoutes from "./routes/AppRoutes";
+import MainPage from "./pages/Login/MainPage";
+import Registro from "./pages/Login/Registro";
+import FormularioRegistro from "./pages/Login/FormularioRegistro";
+import Login from "./pages/Login/Login";
+import ResetPassword from "./pages/Login/ResetPassword";
+import ChangePassword from "./pages/Login/ChangePassword";
+import Courses from "./pages/Aprendiz/curso/Courses";
+import CourseLayoutPage from "./pages/Aprendiz/curso/CourseLayoutPage";
+import CourseContentPage from "./pages/Aprendiz/curso/CourseContentPage";
+import CourseMentoriasPage from "./pages/Aprendiz/curso/CourseMentoriasPage";
+import CourseProyectsPage from "./pages/Aprendiz/curso/CourseProyectsPage";
+import MentoriasPage from "./pages/Aprendiz/mentorias/MentoriasPage";
+import ProyectsPage from "./pages/Aprendiz/proyectos/ProyectsPage";
+import SearchPage from "./pages/SearchPage";
+import DashboardPage from "./pages/DashboardPage";
 
 /**
  * El Layout principal de la aplicación.
  * Contiene los elementos persistentes como el Sidebar y el Header.
  * El componente <Outlet> renderizará el componente de la ruta actual.
  */
-function Layout() {
+export function Layout() {
   const location = useLocation();
 
-
-  const showHeader = /^\/(courses|mentorias|proyectos)(\/\d+)?$/.test(
+  const showHeader = /^\/(dashboard|courses|mentorias|proyectos)(\/\d+)?$/.test(
     location.pathname
   );
   const showFooter = showHeader;
@@ -60,6 +74,7 @@ export default function App() {
         <Route path="cambiar-password" element={<ChangePassword />} />
 
         {/* Cursos y sus subrutas */}
+        <Route path="dashboard" element={<DashboardPage />} />
         <Route path="courses" element={<Courses />} />
         <Route path="courses/:courseId" element={<CourseLayoutPage />}>
           <Route path="content" element={<CourseContentPage />} />
