22cf950990647e8602658c605a9eae78a3c2ebb7
Alejandro <alejodev2022@gmail.com>
2025-06-23 00:33:27 -0500

    refactor(MainPage.jsx): Actualizar las importaciones para utilizar alias y mejorar la organización del código

--- frontend/SkillLink-Frontend/src/pages/Login/MainPage.jsx
+++ frontend/SkillLink-Frontend/src/pages/Login/MainPage.jsx
@@ -1,11 +1,11 @@
-import Navbar from "../../components/Main/Navbar";
-import Hero from "../../components/Main/Hero";
-import CourseList from "../../components/Main/CourseList";
-import MentorList from "../../components/Main/MentorList";
-import Testimonios from "../../components/Main/Testimonios";
-import InfoCards from "../../components/Main/InfoCards";
-import CallToAction from "../../components/Main/CallToAction";
-import Footer from "../../components/Main/Footer";
+import Navbar from "@/components/Main/Navbar";
+import Hero from "@/components/Main/Hero";
+import CourseList from "@/components/Main/CourseList";
+import MentorList from "@/components/Main/MentorList";
+import Testimonios from "@/components/Main/Testimonios";
+import InfoCards from "@/components/Main/InfoCards";
+import CallToAction from "@/components/Main/CallToAction";
+import Footer from "@/components/Main/Footer";
 
 export default function MainPage() {
   return (
