cea5e444f30ab9c95dd610be8c01e1301b04b991
Alejandra Toloza <judytoloza@hotmail.com>
2025-06-23 22:01:17 +0200

    fix(pagina): corregir rutas de importación en el componente Error404

--- SkillLink-Frontend/src/pages/Error404/Error404.jsx
+++ SkillLink-Frontend/src/pages/Error404/Error404.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import Sidebar from '../components/Sidebar';
-import Topbar from '../components/Topbar';
-import Error404Img from '../assets/imagen/Error404.png';
-import '../styles/Error404.css'
-import '../styles/App.css';
+import Sidebar from '../../components/comun/Sidebar';
+import Topbar from '../../components/comun/Header';
+import Error404Img from '../../assets/img/Error404.png';
+import '../../styles/Error404.css'
 
 function Error404() {
     return (
