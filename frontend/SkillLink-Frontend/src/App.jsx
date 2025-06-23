ee107971739f6948de11f8c149143eb2d3692a6e
Alejandro <alejodev2022@gmail.com>
2025-06-23 00:33:00 -0500

    refactor(App.jsx): Simplificar importaciones y reestructurar el componente Layout para mejorar la legibilidad y organización del código

--- frontend/SkillLink-Frontend/src/App.jsx
+++ frontend/SkillLink-Frontend/src/App.jsx
@@ -1,15 +1,9 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Outlet,
-  useLocation,
-} from "react-router-dom";
+import { Routes, Route, Outlet, useLocation } from "react-router-dom";
 import Header from "@/components/comun/Header";
-import Footer from "@/components/comun/Footer";
-import Sidebar from "@/components/comun/Sidebar";
-import CurseTabs from "@/components/Aprendiz/Curso/CourseTabs";
+import Footer from "@/components/Main/Footer";
+import Sidebar from "@/components/Sidebar/Sidebar";
+import CurseTabs from "@/components/Curso/CourseTabs";
 import AppRoutes from "@/routes/AppRoutes";
 
 import MainPage from "./pages/Login/MainPage";
@@ -28,7 +22,7 @@ import MentoriasPage from "./pages/mentorias/MentoriasPage";
 import ProyectsPage from "./pages/proyectos/ProyectsPage";
 import SearchPage from "./pages/SearchPage";
 
-function Layout() {
+export function Layout() {
   const location = useLocation();
 
   const showHeader = /^\/(courses|mentorias|proyectos)(\/\d+)?$/.test(
@@ -59,34 +53,32 @@ function Layout() {
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* Login / Registro */}
-          <Route index element={<MainPage />} />
-          <Route path="registro-basico" element={<Registro />} />
-          <Route path="registro" element={<FormularioRegistro />} />
-          <Route path="login" element={<Login />} />
-          <Route path="restablecer" element={<ResetPassword />} />
-          <Route path="cambiar-password" element={<ChangePassword />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Login / Registro */}
+        <Route index element={<MainPage />} />
+        <Route path="registro-basico" element={<Registro />} />
+        <Route path="registro" element={<FormularioRegistro />} />
+        <Route path="login" element={<Login />} />
+        <Route path="restablecer" element={<ResetPassword />} />
+        <Route path="cambiar-password" element={<ChangePassword />} />
 
-          {/* Cursos y sus subrutas */}
-          <Route path="courses" element={<Courses />} />
-          <Route path="courses/:courseId" element={<CourseLayoutPage />}>
-            <Route path="content" element={<CourseContentPage />} />
-            <Route path="mentorias" element={<CourseMentoriasPage />} />
-            <Route path="proyectos" element={<CourseProyectsPage />} />
-          </Route>
-
-          {/* Otras secciones */}
-          <Route path="mentorias" element={<MentoriasPage />} />
-          <Route path="proyectos" element={<ProyectsPage />} />
-          <Route path="search" element={<SearchPage />} />
-
-          {/* 404 */}
-          <Route path="*" element={<div>Página no encontrada</div>} />
+        {/* Cursos y sus subrutas */}
+        <Route path="courses" element={<Courses />} />
+        <Route path="courses/:courseId" element={<CourseLayoutPage />}>
+          <Route path="content" element={<CourseContentPage />} />
+          <Route path="mentorias" element={<CourseMentoriasPage />} />
+          <Route path="proyectos" element={<CourseProyectsPage />} />
         </Route>
-      </Routes>
-    </Router>
+
+        {/* Otras secciones */}
+        <Route path="mentorias" element={<MentoriasPage />} />
+        <Route path="proyectos" element={<ProyectsPage />} />
+        <Route path="search" element={<SearchPage />} />
+
+        {/* 404 */}
+        <Route path="*" element={<div>Página no encontrada</div>} />
+      </Route>
+    </Routes>
   );
 }
