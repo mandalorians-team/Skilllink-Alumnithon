e2beb16164044d7035f912eb6fdb54ef0bb4f1a3
Alejandro <alejodev2022@gmail.com>
2025-06-23 12:29:12 -0500

    feat(DashboardPage): reemplazar contenido estático por el componente Dashboard, mejorando la modularidad y la estructura del código.

--- frontend/SkillLink-Frontend/src/pages/DashboardPage.jsx
+++ frontend/SkillLink-Frontend/src/pages/DashboardPage.jsx
@@ -1,24 +1,13 @@
 import React from "react";
-import { useAuth } from "@/context/AuthContext";
-import useDocumentTitle from "@/hooks/useDocumentTitle";
+
+import Dashboard from "@/components/Main/Dashboard";
+
 
 export default function DashboardPage() {
-  useDocumentTitle("Panel Principal");
-  const { user, isAuthenticated } = useAuth();
-
   return (
     <div>
-      <h1 className="text-3xl font-bold text-black">Bienvenido a SkillLink</h1>
-      {isAuthenticated ? (
-        <p className="text-black font-bold font-orbitron mt-2">
-          Hola, <span className="font-bold">{user.name}</span>. Usa el menú
-          lateral para navegar por la aplicación.
-        </p>
-      ) : (
-        <p className="text-black mt-2">
-          Por favor, inicia sesión para acceder a tus cursos y mentorías.
-        </p>
-      )}
+      
+      <Dashboard />
     </div>
   );
 }
