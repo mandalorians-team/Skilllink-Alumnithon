2154e0767406200fbc55aae6babdcc2de9943640
Alejandro <alejodev2022@gmail.com>
2025-06-23 12:29:22 -0500

    feat(Login): implementar formulario de inicio de sesión con manejo de estado y validación de errores, mejorando la experiencia del usuario.

--- frontend/SkillLink-Frontend/src/pages/Login/Login.jsx
+++ frontend/SkillLink-Frontend/src/pages/Login/Login.jsx
@@ -3,10 +3,27 @@ import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Main/Navbar";
 import Footer from "../../components/Main/Footer";
 import "../../index.css";
+import { useState } from "react";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const { login } = useAuth();
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError(null);
+    try {
+      await login(email, password);
+      navigate("/dashboard");
+    } catch (error) {
+      setError("Email o contraseña incorrectos");
+    }
+  };
   return (
     <div className="min-h-screen flex flex-col bg-[#B8CFDF] font-sans">
       {/* HEADER */}
@@ -31,51 +48,56 @@ export default function Login() {
               Inicia sesión y únete a la aventura SkillLink.
             </p>
 
-            <input
-              type="email"
-              placeholder="tu.email@ejemplo.com"
-              className="w-full mb-3 px-4 py-2 rounded bg-black border border-[#393D47] text-[#8C8D8B] text-sm focus:outline-none focus:ring-2 focus:ring-[#799EB8] focus:border-[#799EB8] transition duration-300"
-            />
-            <input
-              type="password"
-              placeholder="Ingresa tu contraseña"
-              className="w-full mb-4 px-4 py-2 rounded bg-black border border-[#393D47] text-[#8C8D8B] text-sm focus:outline-none focus:ring-2 focus:ring-[#799EB8] focus:border-[#799EB8] transition duration-300"
-            />
-
-            <button className="w-full py-2 bg-gradient-to-r from-[#799EB8] to-[#678a9d] text-white rounded-md mb-4 text-sm font-semibold hover:scale-105 hover:brightness-110 transition-all duration-500 relative overflow-hidden">
-              <span className="relative z-10">Iniciar Sesión</span>
-              <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/10 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-500 blur-sm"></div>
-            </button>
-
-            {[
-              { img: "youtube.png", label: "Youtube" },
-              { img: "apple.png", label: "Apple" },
-              { img: "facebook.png", label: "Facebook" }
-            ].map(({ img, label }) => (
+            <form onSubmit={handleLogin}>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="tu.email@ejemplo.com"
+                className="w-full mb-3 px-4 py-2 rounded bg-black border border-[#393D47] text-[#8C8D8B] text-sm focus:outline-none focus:ring-2 focus:ring-[#799EB8] focus:border-[#799EB8] transition duration-300"
+              />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Ingresa tu contraseña"
+                className="w-full mb-4 px-4 py-2 rounded bg-black border border-[#393D47] text-[#8C8D8B] text-sm focus:outline-none focus:ring-2 focus:ring-[#799EB8] focus:border-[#799EB8] transition duration-300"
+              />
               <button
-                key={label}
-                className="w-full py-2 mb-2 border text-sm rounded-md flex items-center justify-center gap-2 bg-white hover:bg-gray-100 hover:scale-105 transition-transform duration-300"
-              >
-                <img src={`/images/${img}`} alt={label} className="h-5 w-5" />{" "}
-                Continuar con {label}
+                type="submit"
+                className="w-full py-2 bg-gradient-to-r from-[#799EB8] to-[#678a9d] text-white rounded-md mb-4 text-sm font-semibold hover:scale-105 hover:brightness-110 transition-all duration-500 relative overflow-hidden">
+                <span className="relative z-10">Iniciar Sesión</span>
+                <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white/10 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-500 blur-sm"></div>
               </button>
-            ))}
 
-            <p
-              className="text-center text-[#799EB8] text-sm hover:underline cursor-pointer mb-2 transition"
-              onClick={() => navigate("/restablecer")}
-            >
-              ¿Olvidaste tu contraseña?
-            </p>
-            <p className="text-center text-white text-sm">
-              ¿No tienes una cuenta?{" "}
-              <span
-                className="text-[#799EB8] hover:underline cursor-pointer transition"
-                onClick={() => navigate("/registro")}
-              >
-                Regístrate
-              </span>
-            </p>
+              {[
+                { img: "github.png", label: "Github" },
+                { img: "google.png", label: "Google" },
+              ].map(({ img, label }) => (
+                <button
+                  key={label}
+                  className="w-full py-2 mb-2 border text-sm rounded-md flex items-center justify-center gap-2 bg-white hover:bg-gray-100 hover:scale-105 transition-transform duration-300">
+                  <img src={`/images/${img}`} alt={label} className="h-5 w-5" />{" "}
+                  Continuar con {label}
+                </button>
+              ))}
+
+              <p
+                className="text-center text-[#799EB8] text-sm hover:underline cursor-pointer mb-2 transition"
+                onClick={() => navigate("/restablecer")}>
+                ¿Olvidaste tu contraseña?
+              </p>
+              <p className="text-center text-white text-sm">
+                ¿No tienes una cuenta?{" "}
+                <span
+                  className="text-[#799EB8] hover:underline cursor-pointer transition"
+                  onClick={() => navigate("/registro")}>
+                  Regístrate
+                </span>
+              </p>
+            </form>
+
+            {error && <p className="text-red-500 text-center">{error}</p>}
           </div>
         </div>
 
