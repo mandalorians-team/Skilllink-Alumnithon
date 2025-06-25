b7a8b454ec5616374d1e4300aa333548e022dd36
Alejandro <alejodev2022@gmail.com>
2025-06-25 10:42:38 -0500

    feat(DashboardPage): agregar nuevo componente DashboardPage que incluye el PanelEstudiante, mejorando la estructura del frontend y la experiencia del usuario.

---
+++ frontend/SkillLink-Frontend/src/pages/Aprendiz/DashboardPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PanelEstudiante from "./PanelEstudiante";
+
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col min-h-screen bg-[#B8CFDF]">
+      <PanelEstudiante />
+    </div>
+  );
+}
