60d8383146799db118e158687eab9bf9d15f637f
Alejandro <alejodev2022@gmail.com>
2025-06-25 10:43:06 -0500

    feat(MainPage): agregar nuevo componente MainPage que incluye Navbar, Hero, CourseList, MentorList, Testimonios, InfoCards, CallToAction y Footer, mejorando la estructura de la página de inicio.

---
+++ frontend/SkillLink-Frontend/src/pages/LandingPage/MainPage.jsx
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Main/Navbar";
+import Hero from "@/components/Main/Hero";
+import CourseList from "@/components/Main/CourseList";
+import MentorList from "@/components/Main/MentorList";
+import Testimonios from "@/components/Main/Testimonios";
+import InfoCards from "@/components/Main/InfoCards";
+import CallToAction from "@/components/Main/CallToAction";
+import Footer from "@/components/Main/Footer";
+
+
+export default function MainPage() {
+  return (
+    <div className="bg-[#b8cfdf] text-[#1a1a1a] font-sans">
+      <Navbar />
+      <Hero />
+      <CourseList />
+      <MentorList />
+      <Testimonios />
+      <InfoCards />
+      <CallToAction />
+      <Footer />
+    </div>
+  );
+}
