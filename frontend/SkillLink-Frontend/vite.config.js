d5e5a7c82c8bfa47d111f8803c21146c9e51e9d3
Alejandro <alejodev2022@gmail.com>
2025-06-26 00:29:27 -0500

    actualizar la configuración del proxy en vite.config.js para redirigir las solicitudes de la API al puerto 8080, mejorando la conexión con el backend.

--- frontend/SkillLink-Frontend/vite.config.js
+++ frontend/SkillLink-Frontend/vite.config.js
@@ -16,13 +16,13 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8081",
+        target: "http://localhost:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
- 
+
   test: {
     globals: true,
     environment: "jsdom",
